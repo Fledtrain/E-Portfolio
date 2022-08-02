a403f554c719d3bfafac264eca85e857d1e36d3b
Joey <Joeyew2014@gmail.com>
2022-08-02 14:45:35 -0400

    Added stuff for hover on mobile for projects

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function toggleDarkMode()
-{
-    document.querySelector('body').classList.toggle("dark-theme")
+function toggleDarkMode() {
+  document.querySelector("body").classList.toggle("dark-theme");
 }
-
+// This snippet will enable hover effects for touchscreens
+document.addEventListener("touchstart", function () {}, true);
