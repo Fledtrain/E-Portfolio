0fca09b6af37e7781f7b8ed073e818f130e33c16
Fledtrain <Joeyew2014@gmail.com>
2023-01-16 21:27:37 -0500

    Testing phase

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-function toggleDarkMode() {
+const toggleDarkMode = () => {
   document.querySelector("body").classList.toggle("dark-theme");
-}
+};
 // This snippet will enable hover effects for touchscreens
-document.addEventListener("touchstart", function () {}, true);
+document.addEventListener("touchstart", function () {}, true); //Dont remove this line
+
+
+// Local Storage
+//Save the dark mode selection to local storage
